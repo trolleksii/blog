1c0d822d5a15fd48c8edd2fdb926a7beab8a6949
trolleksii <trolleksii@gmail.com>
2018-03-26 17:53:24 +0300

    profile views and tests for them

---
+++ blog/apps/profiles/tests/test_views.py
@@ -0,0 +1,125 @@
+from django.shortcuts import reverse
+from django.test import TestCase
+
+from apps.authentication.models import User
+
+
+class ProfileViewTests(TestCase):
+
+    fixtures = ['db.json']
+
+    def test_get_profile_info(self):
+        # Kenny makes request to see Stan's profile
+        token = User.objects.get(username='kenny').token
+        headers = {
+            'HTTP_AUTHORIZATION': 'Token ' + token
+        }
+        response = self.client.get(
+            reverse('profiles:profile_view', kwargs={'username': 'stan'}),
+            **headers
+        )
+        profile = response.data.get('profile', None)
+        self.assertEqual(profile['username'], 'stan')
+        self.assertTrue(profile['following'])
+
+    def test_get_profile_info_unauthenticated(self):
+        response = self.client.get(
+            reverse('profiles:profile_view', kwargs={'username': 'stan'})
+        )
+        profile = response.data.get('profile', None)
+        self.assertEqual(profile['username'], 'stan')
+        self.assertFalse(profile['following'])
+
+
+class ProfileFollowViewTests(TestCase):
+
+    fixtures = ['db.json']
+
+    def test_follow_self(self):
+        token = User.objects.get(username='kyle').token
+        headers = {
+            'HTTP_AUTHORIZATION': 'Token ' + token
+        }
+        response = self.client.post(
+            reverse('profiles:follow_view', kwargs={'username': 'kyle'}),
+            data={},
+            content_type='application/json',
+            **headers
+        )
+        data = response.data.get('profile', None)
+        self.assertFalse(data['following'])
+
+    def test_follow_profile(self):
+        token = User.objects.get(username='kyle').token
+        headers = {
+            'HTTP_AUTHORIZATION': 'Token ' + token
+        }
+        response = self.client.post(
+            reverse('profiles:follow_view', kwargs={'username': 'eric'}),
+            data={},
+            content_type='application/json',
+            **headers
+        )
+        data = response.data.get('profile', None)
+        self.assertTrue(data['following'])
+
+    def test_unfollow(self):
+        user = User.objects.get(username='kenny')
+        followee = 'kyle'
+        user_has_followee = len(user.profile.followees.filter(
+            user__username=followee)) == 1
+        self.assertTrue(user_has_followee)
+        headers = {
+            'HTTP_AUTHORIZATION': 'Token ' + user.token
+        }
+        response = self.client.delete(
+            reverse('profiles:follow_view', kwargs={'username': followee}),
+            data={},
+            content_type='application/json',
+            **headers
+        )
+        data = response.data.get('profile', None)
+        self.assertFalse(data['following'])
+
+    def test_unauthenticated(self):
+        response = self.client.post(
+            reverse('profiles:follow_view', kwargs={'username': 'kyle'}),
+            data={},
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 403)
+
+
+class ProfileFolloweesViewTests(TestCase):
+
+    fixtures = ['db.json']
+
+    def test_get_empty_follows(self):
+        token = User.objects.get(username='eric').token
+        headers = {
+            'HTTP_AUTHORIZATION': 'Token ' + token
+        }
+        response = self.client.get(
+            reverse('profiles:followees_view'),
+            **headers
+        )
+        followees = response.data.get('followees', None)
+        self.assertEqual(followees, [])
+
+    def test_get_follows(self):
+        token = User.objects.get(username='kenny').token
+        headers = {
+            'HTTP_AUTHORIZATION': 'Token ' + token
+        }
+        response = self.client.get(
+            reverse('profiles:followees_view'),
+            **headers
+        )
+        followees = response.data.get('followees', None)
+        self.assertEqual(set(followees), set(['stan', 'kyle', 'eric']))
+
+    def test_get_unauthenticated(self):
+        response = self.client.get(
+            reverse('profiles:followees_view'),
+        )
+        self.assertEqual(response.status_code, 403)
--- blog/apps/profiles/views.py
+++ blog/apps/profiles/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import permissions, status, views
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Profile
+from .serializers import ProfileFolloweesSerializer, ProfileSerializer
+
+
+class ProfileView(views.APIView):
+    """
+    Returns profile information by username parameter.
+    """
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = ProfileSerializer
+
+    def get(self, request, username, *args, **kwargs):
+        try:
+            profile = Profile.objects.get(user__username=username)
+        except ObjectDoesNotExist:
+            raise NotFound('profile with this username does not exists')
+        serializer = self.serializer_class(profile, context={'request': request})
+        return Response({'profile': serializer.data}, status=status.HTTP_200_OK)
+
+
+class ProfileFollowView(views.APIView):
+    """
+    Handles profile fllows/unfollows. Request maker is the follower, followee
+    is obtained by username parameter.
+    """
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = ProfileSerializer
+    model = Profile
+
+    def post(self, request, username, *args, **kwargs):
+        follower = request.user.profile
+        try:
+            followee = self.model.objects.get(user__username=username)
+        except ObjectDoesNotExist:
+            raise NotFound("profile '{}' does not exists".format(username))
+        follower.follow(followee)
+        serializer = self.serializer_class(followee, context={'request': request})
+        return Response({'profile': serializer.data}, status=status.HTTP_200_OK)
+
+    def delete(self, request, username, *args, **kwargs):
+        follower = request.user.profile
+        try:
+            followee = self.model.objects.get(user__username=username)
+        except ObjectDoesNotExist:
+            raise NotFound('profile {} does not exists'.format(username))
+        follower.unfollow(followee)
+        serializer = self.serializer_class(followee, context={'request': request})
+        return Response({'profile': serializer.data}, status=status.HTTP_200_OK)
+
+
+class ProfileFolloweesView(views.APIView):
+    """
+    Shows list of all followees of currently authenticated user.
+    """
+    permission_classes = (permissions.IsAuthenticated, )
+    serializer_class = ProfileFolloweesSerializer
+
+    def get(self, request, *args, **kwargs):
+        user_profile = request.user.profile
+        serializer = self.serializer_class(user_profile)
+        return Response(serializer.data, status=status.HTTP_200_OK)
