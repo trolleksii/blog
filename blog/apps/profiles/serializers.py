7b5a9b83715d44f00707706dadd99151afe40e09
trolleksii <trolleksii@gmail.com>
2018-03-26 15:43:10 +0300

    profile-relates serializers

---
+++ blog/apps/profiles/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+
+from .models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    """
+    Read-only serializer that returns information about the profile, and checks
+    if request owner follows it.
+    """
+    username = serializers.CharField(source='user.username')
+    following = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Profile
+        fields = ['username', 'about', 'pic', 'following']
+        read_only_fields = ['username', 'about', 'pic', 'following']
+
+    def get_following(self, obj):
+        """
+        Check if request owner follows profile viewed(obj).
+        """
+        request = self.context.get('request', None)
+        print(request)
+        if request and request.user.is_authenticated:
+            return request.user.profile.has_in_followees(obj)
+        return False
+
+
+class ProfileRelatedField(serializers.RelatedField):
+
+    def to_representation(self, value):
+        return value.user.username
+
+
+class ProfileFolloweesSerializer(serializers.ModelSerializer):
+    """
+    Read-only serializer that returns list of profile followees usernames.
+    """
+    followees = ProfileRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = ['followees', ]
