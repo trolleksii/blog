e176c83703d3422250937d41976f03b4b538e3f1
trolleksii <trolleksii@gmail.com>
2018-03-27 18:12:49 +0300

    added field and methods to add posts to favorites

--- blog/apps/profiles/models.py
+++ blog/apps/profiles/models.py
@@ -8,17 +8,27 @@ class Profile(models.Model):
     about = models.TextField(max_length=500)
     pic = models.URLField(blank=True)
     followees = models.ManyToManyField('self', related_name='followers', symmetrical=False)
+    favorites = models.ManyToManyField('posts.Post', related_name='favorited_by')
 
     def __str__(self):
         return self.user.username
+
+    def favorite(self, post):
+        self.favorites.add(post)
 
     def follow(self, profile):
         if profile != self:
             self.followees.add(profile)
 
+    def unfavorite(self, post):
+        self.favorites.remove(post)
+
     def unfollow(self, profile):
         if profile != self:
             self.followees.remove(profile)
+
+    def has_in_favorites(self, post):
+        return self.favorites.filter(pk=post.pk).exists()
 
     # follower --> me --> followee
     def has_in_followees(self, profile):
