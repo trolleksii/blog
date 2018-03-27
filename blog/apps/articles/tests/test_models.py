bb1521ae772e2b5deac638c723f8cf14079b557d
trolleksii <trolleksii@gmail.com>
2018-03-27 11:37:36 +0300

    models tests and fixtures for them

--- blog/apps/articles/tests/test_models.py
+++ blog/apps/articles/tests/test_models.py
@@ -1,38 +1,87 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from apps.articles.models import Article, Comment, Tag
 
-from apps.profiles.models import Profile, User
+from apps.profiles.models import Profile
 
 
 class ArticleModelTests(TestCase):
 
-    def setUp(self):
-        user = User.objects.create_user(
-            username='kenny',
-            password='qwerty123',
-            email='kenny@spmail.com'
+    fixtures = ['db.json']
+
+    def test_create_empty_title(self):
+        author = Profile.objects.get(user__username='kenny')
+        article = Article(
+            title='',
+            slug='second-article',
+            body='article body',
+            author=author
         )
-        self.profile = Profile.objects.create(
-            user=user,
-            about='I am Kenny',
-            pic='http://static.domain.com/images/kenny.png'
+        with self.assertRaises(ValidationError):
+            article.full_clean()
+
+    def test_create_empty_slug(self):
+        author = Profile.objects.get(user__username='kenny')
+        article = Article(
+            title='Second article',
+            slug='',
+            body='article body',
+            author=author
         )
+        with self.assertRaises(ValidationError):
+            article.full_clean()
+
+    def test_create_empty_body(self):
+        author = Profile.objects.get(user__username='kenny')
+        article = Article(
+            title='Second article',
+            slug='second-article',
+            body='',
+            author=author
+        )
+        with self.assertRaises(ValidationError):
+            article.full_clean()
+
+    def test_create_no_author(self):
+        article = Article(
+            title='Second article',
+            slug='second-article',
+            body='article body',
+        )
+        with self.assertRaises(ValidationError):
+            article.full_clean()
+
+    def test_create_duplicate_slug_article(self):
+        author = Profile.objects.get(user__username='kenny')
+        article = Article(
+            title='some title',
+            slug='my-very-first-article',
+            body='article body',
+            author=author
+        )
+        with self.assertRaises(ValidationError):
+            article.full_clean()
 
     def test_str(self):
+        author = Profile.objects.get(user__username='kenny')
         article = Article.objects.create(
             slug='the-first-article',
             title='The first article',
             body='blah blah blah...',
-            author=self.profile
+            author=author
         )
         self.assertEqual(str(article), article.title)
 
 
 class CommentModelTests(TestCase):
 
+    fixtures = ['db.json']
+
     def test_str(self):
-        pass
+        comment = Comment.objects.first()
+        self.assertIsNotNone(comment)
+        self.assertEqual(str(comment), comment.title)
 
 
 class TagModelTests(TestCase):
@@ -43,3 +92,21 @@ class TagModelTests(TestCase):
             body='New Tag'
         )
         self.assertEqual(str(tag), tag.body)
+
+    def test_add_duplicate_tag(self):
+        Tag.objects.create(
+            slug='new-tag',
+            body='New Tag'
+        )
+        duplicate_slug = Tag(
+            slug='new-tag',
+            body='Newest Tag'
+        )
+        duplicate_body = Tag(
+            slug='new-tag',
+            body='Newest Tag'
+        )
+        with self.assertRaises(ValidationError):
+            duplicate_slug.full_clean()
+        with self.assertRaises(ValidationError):
+            duplicate_body.full_clean()
