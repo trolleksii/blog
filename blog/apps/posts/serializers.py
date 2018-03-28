dcaf8ad8321d661005f77f360b41fd19cfc996fb
trolleksii <trolleksii@gmail.com>
2018-03-28 18:54:56 +0300

    made serizlisers, working on tests

---
+++ blog/apps/posts/serializers.py
@@ -0,0 +1,113 @@
+from django.utils.text import slugify
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework.settings import api_settings
+
+from apps.posts.models import Comment, Post, Tag
+from apps.profiles.serializers import ProfileSerializer
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    createdAt = serializers.DateTimeField(source='created_at')
+    modifiedAt = serializers.DateTimeField(source='modified_at')
+
+    class Meta:
+        model = Comment
+        fields = ['title', 'body', 'createdAt', 'modifiedAt', 'author']
+        read_only_fields = ['createdAt', 'modifiedAt', 'author']
+
+    def validate(self, args):
+        user = self.context.get('user', None)
+        if user is None or not user.is_authenticated:
+            message = 'user not authenticated'
+            raise ValidationError({
+                api_settings.NON_FIELD_ERRORS_KEY: [message]
+            }, code='invalid')
+        post = self.context.get('post', None)
+        if post is None:
+            message = 'post not specified'
+            raise ValidationError({
+                api_settings.NON_FIELD_ERRORS_KEY: [message]
+            }, code='invalid')
+        args['author'] = user.profile
+        args['post'] = post
+        return args
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['body', ]
+
+    def to_internal_value(self, data):
+        if not isinstance(data, str):
+            message = 'expected list of strings, got list of {}'.format(type(data))
+            raise ValidationError({
+                api_settings.NON_FIELD_ERRORS_KEY: [message]
+            }, code='invalid')
+        return {
+            'body': data,
+            'slug': slugify(data)
+        }
+
+    def to_representation(self, obj):
+        return obj.body
+
+    def create(self, validated_data):
+        return self.Meta.model.objects.get_or_create(**validated_data)[0]
+
+
+class PostSerializer(serializers.ModelSerializer):
+    createdAt = serializers.SerializerMethodField(method_name='get_created_at')
+    updatedAt = serializers.SerializerMethodField(method_name='get_updated_at')
+    likes = serializers.SerializerMethodField()
+    dislikes = serializers.SerializerMethodField()
+    favorited = serializers.SerializerMethodField()
+    author = ProfileSerializer(read_only=True)
+    tagList = TagSerializer(many=True, required=False, source='tags')
+
+    class Meta:
+        model = Post
+        fields = [
+            'slug', 'title', 'body', 'tagList', 'createdAt', 'updatedAt',
+            'favorited', 'likes', 'dislikes', 'author'
+        ]
+        read_only_fields = ['slug']
+
+    def validate(self, args):
+        user = self.context.get('user', None)
+        if user is None or not user.is_authenticated:
+            message = 'user not authenticated'
+            raise ValidationError({
+                api_settings.NON_FIELD_ERRORS_KEY: [message]
+            }, code='invalid')
+        args['author'] = user.profile
+        # TODO generate unique slugs
+        args['slug'] = slugify(args['title'])
+        return args
+
+    def create(self, validated_data):
+        tag_data_list = validated_data.pop('tags', [])
+        post = Post.objects.create(**validated_data)
+        for tag_data in tag_data_list:
+            tag = Tag.objects.get_or_create(**tag_data)[0]
+            post.tags.add(tag)
+        return post
+
+    def get_likes(self, post):
+        return post.get_likes()
+
+    def get_dislikes(self, post):
+        return post.get_dislikes()
+
+    def get_favorited(self, post):
+        user = self.context.get('user', None)
+        if user and user.is_authenticated:
+            return user.profile.has_in_favorites(post)
+        return False
+
+    def get_created_at(self, post):
+        return str(post.created_at)
+
+    def get_updated_at(self, post):
+        return str(post.modified_at)
---
+++ blog/apps/posts/tests/test_serializers.py
@@ -0,0 +1,128 @@
+from django.test import TestCase
+
+from apps.authentication.models import User
+from apps.posts.models import Post, Tag
+from apps.posts.serializers import PostSerializer
+
+
+class CommentSerializerTests(TestCase):
+
+    def test_serialize_comment(self):
+        pass
+
+    def test_serialize_multiple_comments(self):
+        pass
+
+    def test_deserialize_comment(self):
+        pass
+
+    def test_deserialize_comment_without_post(self):
+        pass
+
+    def test_deserialize_comment_without_author(self):
+        pass
+
+    def test_deserialize_empty_comment(self):
+        pass
+
+
+class PostSerializerTests(TestCase):
+
+    fixtures = ['db.json']
+
+    def test_serialize_post(self):
+        post = Post.objects.first()
+        serializer = PostSerializer(post)
+        self.assertEqual(post.slug, serializer.data['slug'])
+        self.assertEqual(post.body, serializer.data['body'])
+        self.assertEqual(post.title, serializer.data['title'])
+        self.assertEqual(post.author.user.username, serializer.data['author']['username'])
+
+    def test_serialize_multiple_posts(self):
+        queryset = Post.objects.all()
+        serializer = PostSerializer(queryset, many=True)
+        self.assertEqual(len(serializer.data), queryset.count())
+
+    def test_deserialize_with_tags(self):
+        user = User.objects.get(username='kenny')
+        serializer = PostSerializer(
+            data={
+                'tagList': ['ctag1', 'ctag2', 'ctag3'],
+                'title': 'Some random title',
+                'body': 'Here goes post body',
+            },
+            context={'user': user}
+        )
+        tags_before = Tag.objects.all().count()
+        self.assertTrue(serializer.is_valid())
+        serializer.save()
+        tags_after = Tag.objects.all().count()
+        self.assertEqual(tags_after, tags_before + 3)
+        post = Post.objects.get(title='Some random title')
+        self.assertEqual(post.tags.count(), 3)
+
+    def test_deserialize_without_tags(self):
+        user = User.objects.get(username='kenny')
+        serializer = PostSerializer(
+            data={
+                'title': 'Some random title',
+                'body': 'Here goes post body',
+            },
+            context={'user': user}
+        )
+        tags_before = Tag.objects.all().count()
+        self.assertTrue(serializer.is_valid())
+        serializer.save()
+        tags_after = Tag.objects.all().count()
+        self.assertEqual(tags_after, tags_before)
+        post = Post.objects.get(title='Some random title')
+        self.assertEqual(post.tags.count(), 0)
+
+    def test_deserialize_without_author(self):
+        serializer = PostSerializer(
+            data={
+                'title': 'Some random title',
+                'body': 'Here goes post body',
+            },
+        )
+        self.assertFalse(serializer.is_valid())
+
+    def test_deserialize_empty_body(self):
+        user = User.objects.get(username='kenny')
+        serializer = PostSerializer(
+            data={
+                'title': 'Some random title',
+                'body': '',
+            },
+            context={'user': user}
+        )
+        self.assertFalse(serializer.is_valid())
+
+    def test_deserialize_empty_title(self):
+        user = User.objects.get(username='kenny')
+        serializer = PostSerializer(
+            data={
+                'title': '',
+                'body': 'Some genius text',
+            },
+            context={'user': user}
+        )
+        self.assertFalse(serializer.is_valid())
+
+
+class TagSerializerTests(TestCase):
+
+    def test_serialize_tag(self):
+        pass
+
+    def test_serialize_multiple_tags(self):
+        pass
+
+    def test_deserialize_tag(self):
+        pass
+
+    def test_serialize_multiple_tag(self):
+        pass
+
+    def test_serialize_multiple_tags_with_duplicates(self):
+        pass
