1bb68ecbfd003f9b1f22e1f88396f657d128d6f8
trolleksii <trolleksii@gmail.com>
2018-03-30 16:13:09 +0300

    added PostViewset and urls for it

--- blog/apps/posts/urls.py
+++ blog/apps/posts/urls.py
@@ -1,14 +1,14 @@
 from django.urls import re_path
 
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 
 from .views import ListTagsAPIView, PostViewSet
 
 app_name = 'posts'
 
-router = DefaultRouter(trailing_slash=False)
+router = SimpleRouter(trailing_slash=False)
 
-router.register(r'posts', PostViewSet, base_name='posts')
+router.register(r'posts', PostViewSet, base_name='post')
 
 urlpatterns = [
     re_path(r'^tags$', ListTagsAPIView.as_view(), name='listtags_view'),
--- blog/apps/posts/views.py
+++ blog/apps/posts/views.py
@@ -1,5 +1,9 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
+
 from rest_framework import status
-from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.decorators import detail_route
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -9,7 +13,7 @@ from .serializers import PostSerializer, TagSerializer
 
 
 class ListTagsAPIView(APIView):
-    permission_classes = (AllowAny, )
+    permission_classes = (AllowAny,)
     serializer_class = TagSerializer
 
     def get(self, request):
@@ -20,6 +24,80 @@ class ListTagsAPIView(APIView):
 
 class PostViewSet(ModelViewSet):
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Post.objects.all()
     lookup_field = 'slug'
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.get('post', {})
+        serializer = self.serializer_class(
+            data=data,
+            context={'user': request.user}
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'user': serializer.data}, status=status.HTTP_201_CREATED)
+
+    def update(self, request, slug, *args, **kwargs):
+        data = request.data.get('post', {})
+        try:
+            instance = self.serializer_class.Meta.model.objects.get(slug=slug)
+        except ObjectDoesNotExist:
+            raise Http404
+        if instance.author != request.user.profile:
+            return Response({}, status=status.HTTP_403_FORBIDDEN)
+        serializer = self.serializer_class(
+            instance,
+            data=data,
+            context={'user': request.user},
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'post': serializer.data}, status=status.HTTP_200_OK)
+
+    def list(self, request, *args, **kwargs):
+        qset = self.get_queryset()
+        serializer = self.serializer_class(
+            qset,
+            context={'user': request.user},
+            many=True
+        )
+        return Response({'posts': serializer.data}, status=status.HTTP_200_OK)
+
+    def destroy(self, request, slug, *args, **kwargs):
+        try:
+            post = self.serializer_class.Meta.model.objects.get(slug=slug)
+        except ObjectDoesNotExist:
+            raise Http404
+        if post.author != request.user.profile:
+            return Response({}, status=status.HTTP_403_FORBIDDEN)
+        post.delete()
+        return Response('', status=status.HTTP_200_OK)
+
+    def retrieve(self, request, slug, *args, **kwargs):
+        try:
+            instance = self.serializer_class.Meta.model.objects.get(slug=slug)
+        except ObjectDoesNotExist:
+            raise Http404
+        serializer = self.serializer_class(
+            instance,
+            context={'user': request.user}
+        )
+        return Response({'post': serializer.data}, status=status.HTTP_200_OK)
+
+    @detail_route(methods=['post', 'delete'], permission_classes=[IsAuthenticated], url_name='favorite')
+    def favorite(self, request, slug):
+        try:
+            post = self.serializer_class.Meta.model.objects.get(slug=slug)
+        except ObjectDoesNotExist:
+            raise Http404
+        if self.request.method == 'POST':
+            request.user.profile.favorite(post)
+        else:
+            request.user.profile.unfavorite(post)
+        serializer = self.serializer_class(
+            post,
+            context={'user': request.user}
+        )
+        return Response({'post': serializer.data}, status=status.HTTP_200_OK)
