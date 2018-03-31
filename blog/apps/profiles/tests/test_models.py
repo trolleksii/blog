97daee162f216f6ebb7400c0a0175a2a95cda8d8
trolleksii <trolleksii@gmail.com>
2018-03-31 17:53:13 +0300

    more tests

--- blog/apps/profiles/tests/test_models.py
+++ blog/apps/profiles/tests/test_models.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 
+from apps.posts.models import Post
+
 from apps.authentication.models import User
+
 from apps.profiles.models import Profile
 
 
@@ -14,40 +17,148 @@ class ProfileModelTests(TestCase):
         self.assertEqual(str(profile), user.username)
 
     def test_follow(self):
-        profile1 = Profile.objects.get(user__username='eric')
-        profile1_followees = profile1.followees.all()
-        self.assertEqual(len(profile1_followees), 0)
-        profile2 = Profile.objects.get(user__username='kyle')
-        profile2_followees = profile2.followees.all()
-        self.assertEqual(len(profile2_followees), 0)
-        profile1.follow(profile2)
-        profile1_followees = profile1.followees.all()
-        profile2_followees = profile2.followees.all()
-        self.assertEqual(len(profile1_followees), 1)
-        self.assertEqual(len(profile2_followees), 0)
-        self.assertEqual(profile1.followees.first(), profile2)
+        kenny = Profile.objects.get(user__username='kenny')
+        kyle = Profile.objects.get(user__username='kyle')
+        followees_before = kyle.followees.all().count()
+        kyle.follow(kenny)
+        followees_after = kyle.followees.all().count()
+        self.assertEqual(followees_after - followees_before, 1)
+        self.assertTrue(
+            kyle.followees.filter(user__username='kenny').exists()
+        )
 
     def test_unfollow(self):
-        profile1 = Profile.objects.get(user__username='kenny')
-        profile2 = Profile.objects.get(user__username='kyle')
-        followees_match = profile1.followees.filter(user__username='kyle')
-        self.assertEqual(len(followees_match), 1)
-        profile1.unfollow(profile2)
-        followees_match = profile1.followees.filter(user__username='kyle')
-        self.assertEqual(len(followees_match), 0)
+        kenny = Profile.objects.get(user__username='kenny')
+        kyle = Profile.objects.get(user__username='kyle')
+        followees_before = kenny.followees.all().count()
+        kenny.unfollow(kyle)
+        followees_after = kenny.followees.all().count()
+        self.assertEqual(followees_before - followees_after, 1)
+        self.assertFalse(
+            kenny.followees.filter(user__username='kyle').exists()
+        )
 
     def test_has_in_followees(self):
-        profile1 = Profile.objects.get(user__username='kenny')
-        profile2 = Profile.objects.get(user__username='kyle')
+        kenny = Profile.objects.get(user__username='kenny')
+        kyle = Profile.objects.get(user__username='kyle')
         self.assertTrue(
-            profile1.has_in_followees(profile2)
+            kenny.has_in_followees(kyle)
         )
         self.assertFalse(
-            profile2.has_in_followees(profile1)
+            kyle.has_in_followees(kenny)
         )
 
     def test_follow_yourself(self):
         profile = Profile.objects.get(user__username='kyle')
         profile.follow(profile)
-        followees = profile.followees.all()
-        self.assertEqual(len(followees), 0)
+        self.assertFalse(
+            profile.followees.filter(user__username='kyle').exists()
+        )
+
+    def test_favorite(self):
+        kenny = Profile.objects.get(user__username='kenny')
+        post = Post.objects.first()
+        followees_before = kenny.favorites.all().count()
+        kenny.favorite(post)
+        followees_after = kenny.favorites.all().count()
+        self.assertEqual(followees_after - followees_before, 1)
+        self.assertTrue(
+            kenny.favorites.filter(slug=post.slug).exists()
+        )
+
+    def test_unfavorite(self):
+        kyle = Profile.objects.get(user__username='kyle')
+        post = Post.objects.first()
+        followees_before = kyle.favorites.all().count()
+        kyle.unfavorite(post)
+        followees_after = kyle.favorites.all().count()
+        self.assertEqual(followees_before - followees_after, 1)
+        self.assertFalse(
+            kyle.favorites.filter(slug=post.slug).exists()
+        )
+
+    def test_has_in_favorites(self):
+        kenny = Profile.objects.get(user__username='kenny')
+        kyle = Profile.objects.get(user__username='kyle')
+        post = Post.objects.first()
+        self.assertTrue(
+            kyle.has_in_favorites(post)
+        )
+        self.assertFalse(
+            kenny.has_in_favorites(post)
+        )
+
+    def test_like(self):
+        kyle = Profile.objects.get(user__username='kyle')
+        post = Post.objects.first()
+        likes_before = post.liked_by.count()
+        kyle.like(post)
+        likes_after = post.liked_by.count()
+        self.assertEqual(likes_after - likes_before, 1)
+
+    def test_like_own_post(self):
+        kenny = Profile.objects.get(user__username='kenny')
+        post = Post.objects.first()
+        likes_before = post.liked_by.count()
+        kenny.like(post)
+        likes_after = post.liked_by.count()
+        self.assertEqual(likes_after, likes_before)
+
+    def test_2xlike(self):
+        kyle = Profile.objects.get(user__username='kyle')
+        post = Post.objects.first()
+        likes_before = post.liked_by.count()
+        kyle.like(post)
+        kyle.like(post)
+        likes_after = post.liked_by.count()
+        self.assertNotEqual(likes_after - likes_before, 2)
+
+    def test_dislike(self):
+        kyle = Profile.objects.get(user__username='kyle')
+        post = Post.objects.first()
+        dislikes_before = post.disliked_by.count()
+        kyle.dislike(post)
+        dislikes_after = post.disliked_by.count()
+        self.assertEqual(dislikes_after - dislikes_before, 1)
+
+    def test_2xdislike(self):
+        kyle = Profile.objects.get(user__username='kyle')
+        post = Post.objects.first()
+        dislikes_before = post.disliked_by.count()
+        kyle.dislike(post)
+        kyle.dislike(post)
+        dislikes_after = post.disliked_by.count()
+        self.assertNotEqual(dislikes_after - dislikes_before, 2)
+
+    def test_like_then_dislike(self):
+        kyle = Profile.objects.get(user__username='kyle')
+        post = Post.objects.first()
+        likes_before = post.liked_by.count()
+        dislikes_before = post.disliked_by.count()
+        kyle.like(post)
+        kyle.dislike(post)
+        likes_after = post.liked_by.count()
+        dislikes_after = post.disliked_by.count()
+        self.assertEqual(likes_after - likes_before, 1)
+        self.assertEqual(dislikes_after - dislikes_before, 0)
+
+    def test_dislike_then_like(self):
+        kyle = Profile.objects.get(user__username='kyle')
+        post = Post.objects.first()
+        likes_before = post.liked_by.count()
+        dislikes_before = post.disliked_by.count()
+        kyle.dislike(post)
+        kyle.like(post)
+        likes_after = post.liked_by.count()
+        dislikes_after = post.disliked_by.count()
+        self.assertEqual(likes_after - likes_before, 0)
+        self.assertEqual(dislikes_after - dislikes_before, 1)
+
+    def test_has_voted_for(self):
+        kyle = Profile.objects.get(user__username='kyle')
+        stan = Profile.objects.get(user__username='stan')
+        post = Post.objects.first()
+        kyle.like(post)
+        stan.dislike(post)
+        self.assertTrue(kyle.has_voted_for(post))
+        self.assertTrue(stan.has_voted_for(post))
