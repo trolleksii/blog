267148cba885c4cfec9b713fb56f39881751c7ea
trolleksii <trolleksii@gmail.com>
2018-03-31 23:13:28 +0300

    exception handler

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -114,7 +114,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-    )
+    ),
+    'EXCEPTION_HANDLER': 'apps.core.exceptions.core_exception_handler',
+    'NON_FIELD_ERRORS_KEY': 'error'
 }
 
 # Internationalization
