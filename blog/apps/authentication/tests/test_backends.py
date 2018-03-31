c84403d6185ed2d1c11b015ab9b67f7e3fd70a17
trolleksii <trolleksii@gmail.com>
2018-03-31 16:03:31 +0300

    added tests for all possible situations

--- blog/apps/authentication/tests/test_backends.py
+++ blog/apps/authentication/tests/test_backends.py
@@ -1,4 +1,9 @@
+from time import sleep
+
 from django.test import TestCase, RequestFactory
+from django.http.request import HttpRequest
+
+from rest_framework.authentication import exceptions
 
 from apps.authentication.models import User
 from apps.authentication.backends import JWTAuthentication
@@ -6,16 +11,72 @@ from apps.authentication.backends import JWTAuthentication
 
 class AuthBackendTests(TestCase):
 
-    def test_correct_use_auth(self):
-        user = User.objects.create(
+    def setUp(self):
+        self.user = User.objects.create_user(
             username='User1',
             password='qwerty123'
         )
-        token = user.token
+
+    def test_all_correct(self):
+        token = self.user.token
         request = RequestFactory().get('/')
         request.META['HTTP_AUTHORIZATION'] = '{} {}'.format(JWTAuthentication.keyword, token)
         auth_user, auth_token = JWTAuthentication().authenticate(request)
         # should be same user
-        self.assertEqual(auth_user, user)
+        self.assertEqual(auth_user, self.user)
         # but different token
         self.assertNotEqual(auth_token, token)
+
+    def test_deleted_user(self):
+        token = self.user.token
+        self.user.delete()
+        request = HttpRequest()
+        request.META['HTTP_AUTHORIZATION'] = '{} {}'.format(JWTAuthentication.keyword, token)
+        with self.assertRaises(exceptions.AuthenticationFailed):
+            user, token = JWTAuthentication().authenticate(request)
+
+    def test_inactive_user(self):
+        token = self.user.token
+        self.user.is_active = False
+        self.user.save()
+        request = HttpRequest()
+        request.META['HTTP_AUTHORIZATION'] = '{} {}'.format(JWTAuthentication.keyword, token)
+        with self.assertRaises(exceptions.AuthenticationFailed):
+            user, token = JWTAuthentication().authenticate(request)
+
+    def test_no_auth_header(self):
+        request = HttpRequest()
+        result = JWTAuthentication().authenticate(request)
+        self.assertIsNone(result)
+
+    def test_header_without_token(self):
+        token = self.user.token
+        self.user.delete()
+        request = HttpRequest()
+        request.META['HTTP_AUTHORIZATION'] = JWTAuthentication.keyword
+        with self.assertRaises(exceptions.AuthenticationFailed):
+            user, token = JWTAuthentication().authenticate(request)
+
+    def test_token_with_spaces(self):
+        token = self.user.token
+        request = HttpRequest()
+        request.META['HTTP_AUTHORIZATION'] = '{} {} {}'.format(
+            JWTAuthentication.keyword,
+            token[:5],
+            token[:6]
+        )
+        with self.assertRaises(exceptions.AuthenticationFailed):
+            user, token = JWTAuthentication().authenticate(request)
+
+    def test_expired_token(self):
+        user = User.objects.create_user(
+            username='TestUser',
+            password='qwerty123',
+            token_valid_for=0
+        )
+        token = user.token
+        request = HttpRequest()
+        request.META['HTTP_AUTHORIZATION'] = '{} {}'.format(JWTAuthentication.keyword, token)
+        sleep(1)
+        with self.assertRaises(exceptions.AuthenticationFailed):
+            user, token = JWTAuthentication().authenticate(request)
