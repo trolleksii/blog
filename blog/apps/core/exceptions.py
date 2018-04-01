94c05c99ff10e072e9296f59559273f67623f9c4
trolleksii <trolleksii@gmail.com>
2018-04-01 12:33:13 +0300

    added oneexception type, needs rewrite

--- blog/apps/core/exceptions.py
+++ blog/apps/core/exceptions.py
@@ -4,16 +4,15 @@ from rest_framework.views import exception_handler
 def core_exception_handler(exc, context):
     response = exception_handler(exc, context)
     handlers = {
-        'NotFound': _handle_generic_error,
+        'NotFound': _handle_not_found_error,
         'ValidationError': _handle_generic_error,
-        'AuthenticationFailed': _handle_auth_error,
-        'NotAuthenticated': _handle_generic_error
+        'AuthenticationFailed': _handle_auth_token_error,
+        'NotAuthenticated': _handle_generic_error,
+        'Http404': _handle_not_found_error
     }
     exception_class = exc.__class__.__name__
     if exception_class in handlers:
         return handlers[exception_class](exc, context, response)
-    # for debug purposes
-    print(exception_class)
     return response
 
 
@@ -24,7 +23,14 @@ def _handle_generic_error(exc, context, response):
     return response
 
 
-def _handle_auth_error(exc, context, response):
+def _handle_not_found_error(exc, context, response):
+    response.data = {
+        'errors': response.data
+    }
+    return response
+
+
+def _handle_auth_token_error(exc, context, response):
     response.data = {
         'errors': {
             'token': response.data['detail']
