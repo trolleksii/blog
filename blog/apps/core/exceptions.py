adb56e7aa683d49b9b4981bf25baccbe0f0979a5
trolleksii <trolleksii@gmail.com>
2018-04-03 18:16:48 +0300

    brought handlers to uniform look

--- blog/apps/core/exceptions.py
+++ blog/apps/core/exceptions.py
@@ -3,17 +3,17 @@ from rest_framework.views import exception_handler
 
 def core_exception_handler(exc, context):
     response = exception_handler(exc, context)
+
     handlers = {
-        'NotFound': _handle_not_found_error,
-        'ValidationError': _handle_generic_error,
         'AuthenticationFailed': _handle_auth_token_error,
-        'NotAuthenticated': _handle_generic_error,
-        'Http404': _handle_not_found_error
+        'Http404': _handle_not_found_error,
+        'NotAuthenticated': _handle_auth_token_error
     }
+
     exception_class = exc.__class__.__name__
     if exception_class in handlers:
         return handlers[exception_class](exc, context, response)
-    return response
+    return _handle_generic_error(exc, context, response)
 
 
 def _handle_generic_error(exc, context, response):
@@ -24,8 +24,10 @@ def _handle_generic_error(exc, context, response):
 
 
 def _handle_not_found_error(exc, context, response):
+    # retrieve model name of requested object
+    model_name = context['view'].serializer_class.Meta.model._meta.verbose_name
     response.data = {
-        'errors': response.data
+        model_name: response.data['detail']
     }
     return response
 
