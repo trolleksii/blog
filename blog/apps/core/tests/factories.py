2b047350c389fa97cacaf8cd944bec55ebfdbc98
trolleksii <trolleksii@gmail.com>
2018-04-03 11:47:49 +0300

    factories for mock data generation for tests

---
+++ blog/apps/core/tests/factories.py
@@ -0,0 +1,50 @@
+import factory
+
+from apps.posts.models import Comment, Post, Tag
+from apps.profiles.models import Profile
+from apps.authentication.models import User
+from apps.core.text import unique_slugify
+
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda x: 'user{}'.format(x))
+    password = 'qwerty123'
+    email = factory.Faker('email')
+
+
+class ProfileFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Profile
+
+    user = factory.SubFactory(UserFactory)
+    about = factory.Faker('sentence', nb_words=4)
+    pic = "http://sourse.somedomain.com/some_pic.png"
+
+
+class PostFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Post
+
+    author = factory.SubFactory(ProfileFactory)
+    body = factory.Faker('sentence', nb_words=4)
+    title = factory.Sequence(lambda x: 'Post Title {}'.format(x))
+    slug = factory.LazyAttribute(lambda x: unique_slugify(x.title))
+
+
+class CommentFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Comment
+
+    title = factory.Sequence(lambda x: 'Comment Title {}'.format(x))
+    body = factory.Faker('sentence', nb_words=4)
+
+
+class TagFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Tag
+
+    body = factory.Sequence(lambda x: 'tag{}'.format(x))
+    slug = factory.LazyAttribute(lambda x: unique_slugify(x.body))
