7763640e774c02247060c4f353e93106e03a43c8
trolleksii <trolleksii@gmail.com>
2018-04-18 21:57:21 +0300

    refactored exception handling

--- blog/apps/core/exceptions.py
+++ blog/apps/core/exceptions.py
@@ -1,4 +1,3 @@
-from rest_framework import status
 from rest_framework.views import exception_handler
 
 
@@ -6,40 +5,16 @@ def core_exception_handler(exc, context):
     exception_class = exc.__class__.__name__
     if exception_class in ['AuthenticationFailed', 'NotAuthenticated']:
         setattr(exc, 'auth_header', 'Bearer realm="Blog"')
-        setattr(exc, 'status_code', status.HTTP_401_UNAUTHORIZED)
     response = exception_handler(exc, context)
-    handlers = {
-        'Http404': _handle_not_found_error,
-        'ValidationError': _handle_validation_error
-    }
-    if exception_class in handlers:
-        return handlers[exception_class](exc, context, response)
-    return _handle_generic_error(exc, context, response)
+    detail = response.data.pop('detail', None)
+    if detail:
+        error_code = exc.get_codes()
+        response.data[error_code] = detail
+    return _wrap_error_response(response)
 
 
-def _handle_generic_error(exc, context, response):
-    exc_code = exc.get_codes()
-    response.data = {
-        'errors': {
-            exc_code: [response.data['detail']]
-        }
-    }
-    return response
-
-
-def _handle_validation_error(exc, context, response):
+def _wrap_error_response(response):
     response.data = {
         'errors': response.data
-    }
-    return response
-
-
-def _handle_not_found_error(exc, context, response):
-    # retrieve model name of requested object
-    model_name = context['view'].serializer_class.Meta.model._meta.verbose_name
-    response.data = {
-        'errors': {
-            model_name: [response.data['detail']]
-        }
     }
     return response
