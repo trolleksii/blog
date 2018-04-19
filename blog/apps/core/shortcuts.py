5b7cf3813c2ffd10acbd3e24e0614d3d0f6f8ba6
trolleksii <trolleksii@gmail.com>
2018-04-19 15:56:00 +0300

    added docstring

--- blog/apps/core/shortcuts.py
+++ blog/apps/core/shortcuts.py
@@ -2,6 +2,15 @@ from rest_framework.exceptions import NotFound
 
 
 def get_object_or_404(model, **kwargs):
+    """
+    Query db for a single object of <model> by one or more search criteria. 
+
+    For example:
+
+    get_object_or_404(User, first_name='John', last_name='Smith').
+
+    Will raise rest_framework.exceptions.NotFound if query fails.
+    """
     try:
         obj = model._default_manager.get(**kwargs)
         return obj
